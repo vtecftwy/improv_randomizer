7462d0d62c6f4231133d1f985ce15b6ef99bcdbf
Etienne Charlier <github@procurasia.com>
2023-10-21 11:10:43 +0800

    Change cast and add widgets

--- utils.py
+++ utils.py
@@ -1,9 +1,15 @@
+import json
 import logging
 import os
 import sys
 from datetime import datetime
 from functools import wraps
+from IPython.display import display
+from ipywidgets import (Text, Textarea, Button, Dropdown, HBox, VBox, IntSlider, Layout, AppLayout, GridspecLayout, Output, HTML, 
+                        interactive, interactive_output, fixed)
 from pathlib import Path
+
+
 
 # Setup path to package and modules
 # TODO: correct this line after transforming this is a package
@@ -59,3 +65,75 @@ def setup_logging(filename=None):
     logthis(f"{'='*50}")
     logthis(f" {'New Session: ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'):^48s}")
     logthis(f"{'='*50}")
+
+
+class WidgetCast:
+    def __init__(self, file=None) -> None:
+        if file is None: 
+            self.p2cast = ROOT/ 'cast.txt'
+        else:
+            self.p2cast = Path(file) if isinstance(file, str) else file
+        assert self.p2cast.is_file()
+
+        with open(self.p2cast, 'r') as fp:
+            lines = fp.readlines()
+            cast_names = [line.removesuffix('\n').strip() for line in lines]
+        self.w = Text(value=', '.join(cast_names), description='Cast', layout=Layout(width='75%'))
+        
+    @property
+    def value(self):
+        return self.w.value
+
+    def update(self, file=None):
+        if file is None: 
+            p2write = self.p2cast
+        else:
+            p2write = Path(file) if isinstance(file, str) else file
+
+        with open(p2write, 'w') as fp:
+            fp.write(self.w.value.replace(', ', '\n'))
+
+    def __call__(self):
+        display(self.w)
+
+    def display(self):
+        display(self.w)
+
+
+class WidgetPrompts:
+    def __init__(self, file=None) -> None:
+        if file is None: 
+            self.p2prompts = ROOT/ 'prompts.txt'
+        else:
+            self.p2prompts = Path(file) if isinstance(file, str) else file
+        assert self.p2prompts.is_file()
+
+        with open(self.p2prompts, 'r') as fp:
+            lines = fp.readlines()
+            self.prompts = [line.removesuffix('\n').strip() for line in lines]
+
+        height = f"{16 * len(self.prompts)}px"
+        self.w = Textarea(
+            value='\n'.join(self.prompts), 
+            description='Prompts', 
+            layout=Layout(width='80%', height=height)
+            )
+                
+    @property
+    def value(self):
+        return self.w.value
+
+    def update(self, file=None):
+        if file is None: 
+            p2write = self.p2prompts
+        else:
+            p2write = Path(file) if isinstance(file, str) else file
+
+        with open(p2write, 'w') as fp:
+            fp.write(self.w.value)
+
+    def __call__(self):
+        display(self.w)
+
+    def display(self):
+        display(self.w)
