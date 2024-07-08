75593dd8119a85f20bdab322a3a0d3a5e2b7affb
Etienne Charlier <github@procurasia.com>
2024-07-08 13:28:11 +0800

    Change setup_logging for debug in file and warning on screen

--- utils.py
+++ utils.py
@@ -41,27 +41,27 @@ def setup_logging(filename=None):
         p2log.touch()
 
     # Configure the root logger
-    logging.basicConfig(
-        level=logging.WARNING,
-        format='%(asctime)s %(message)s',
-        datefmt='%H:%M:%S',
-        )
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG) #Set the root logger to catpure all levels of logs
+
+    # Create a formatter
+    formatter = logging.Formatter('%(asctime)s: %(message)s', datefmt='%H:%M:%S')
+
     # Create a file handler to log to a file
-    file_handler = logging.FileHandler(
-        filename=p2log,
-        mode='a',   
-        encoding='utf-8'
-        )
+    file_handler = logging.FileHandler(filename=p2log, mode='a', encoding='utf-8')
     file_handler.setLevel(logging.DEBUG)  # Set the log level for the file handler
-    file_handler.setFormatter(
-        fmt=logging.Formatter(
-            fmt='%(asctime)s %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S'
-            )    
-        )
-    # Add the file handler to the root logger
-    logging.getLogger('').addHandler(file_handler)
-    print('logging setup done')
+    file_handler.setFormatter(formatter)
+
+    # Create a stream handler for console output
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.WARNING)  # Set the log level for the console handler
+    console_handler.setFormatter(formatter)
+
+    # Add both handlers to the root logger
+    root_logger.addHandler(file_handler)
+    root_logger.addHandler(console_handler)
+
+    print('Logging setup done')
 
 def get_config():
     """Identifies the config file, loads it and returns a config dictionary."""
